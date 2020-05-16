361f4140b32ae8e6d5ecccf4991ae85cf932fcee
Marwen Arfa <marwen.arfa@gmail.com>
2020-05-16 16:23:40 +0200

    feat: welcome page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,36 @@
 import React from "react";
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
 import { Route, Switch, Router } from "react-router-dom";
+import { push } from "redux-first-history";
 
 import "./App.css";
 
 // routes
-import Welcome from "./Welcome";
 import Login from "./pages/login";
+import Welcome from "./pages/welcome";
 import { history, store } from "./store";
+
+export {};
+declare global {
+    interface Window {
+        navigate: any;
+    }
+}
+// tslint:disable-next-line
+window.navigate = (t: any) => store.dispatch(push(t));
 
 function App() {
     return (
         <Provider store={store}>
             <Router history={history}>
                 <Switch>
-                    <Route
-                        path="/"
-                        component={Welcome}
-                        strict={true}
-                        exact={true}
-                    />
                     <Route path="/login" component={Login} exact key="create" />
+                    <Route
+                        path="/welcome"
+                        component={Welcome}
+                        exact
+                        key="create"
+                    />
                     ,{/* Add your routes here */}
                     <Route render={() => <h1>Not Found</h1>} />
                 </Switch>
