2c1d94fd1335a029a8b39956d911a265a2693bb9
Marwen Arfa <marwen.arfa@gmail.com>
2020-05-16 15:09:17 +0200

    feat: setup easy-peasy store

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,21 @@
 import React from "react";
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import { reducer as form } from "redux-form";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Router } from "react-router-dom";
 import createBrowserHistory from "history/createBrowserHistory";
-import {
-    ConnectedRouter,
-    connectRouter,
-    routerMiddleware,
-} from "connected-react-router";
-import "./App.css";
 
-// reducers
-import user from "./reducers/user/";
+import "./App.css";
 
 // routes
 import Welcome from "./Welcome";
-import userRoutes from "./routes/user";
+import { store } from "./store";
+import Login from "./pages/login";
 
 const history = createBrowserHistory();
-const store = createStore(
-    combineReducers({
-        router: connectRouter(history),
-        form,
-        /* Add your reducers here */
-        user,
-    }),
-    composeWithDevTools(applyMiddleware(routerMiddleware(history), thunk))
-);
 
 function App() {
     return (
         <Provider store={store}>
-            <ConnectedRouter history={history}>
+            <Router history={history}>
                 <Switch>
                     <Route
                         path="/"
@@ -42,11 +23,11 @@ function App() {
                         strict={true}
                         exact={true}
                     />
-                    {/* Add your routes here */}
-                    {userRoutes}
+                    <Route path="/login" component={Login} exact key="create" />
+                    ,{/* Add your routes here */}
                     <Route render={() => <h1>Not Found</h1>} />
                 </Switch>
-            </ConnectedRouter>
+            </Router>
         </Provider>
     );
 }
