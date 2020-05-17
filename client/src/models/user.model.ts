7330b1499f37d2b9b50f3547fd9a48248764c4c5
Wassim Arfa <wassim.arfa@gmail.com>
2020-05-17 19:23:13 +0000

    Login + save token

--- client/src/models/user.model.ts
+++ client/src/models/user.model.ts
@@ -3,11 +3,15 @@ import { Action, action, Thunk, thunk } from "easy-peasy";
 import { InjectionModel } from "./injection.model";
 import { User } from "../interfaces/user";
 import { UserRegister } from "../interfaces/user.register";
+import { Auth } from "../interfaces/auth";
+import { Token } from "../interfaces/token";
 
 export interface UserModel {
     user: User;
     savedUser: Action<UserModel, User>;
     register: Thunk<UserModel, UserRegister, InjectionModel>;
+    login: Thunk<UserModel, Auth, InjectionModel>;
+    savedToken: Action<UserModel, Token>;
 }
 
 export const userModel: UserModel = {
@@ -23,5 +27,12 @@ export const userModel: UserModel = {
     register: thunk(async (actions, payload: UserRegister, { injections }) => {
         const user: User = await injections.user.register(payload);
         actions.savedUser(user);
+    }),
+    savedToken: action((state, payload: Token) => {
+        localStorage.setItem("token", payload.token);
+    }),
+    login: thunk(async (actions, payload: Auth, { injections }) => {
+        const token: Token = await injections.user.login(payload);
+        actions.savedToken(token);
     }),
 };
